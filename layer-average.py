3b94163418e891f9e9385bf8e9799b971568d88c
Oona <okraisan@cs.helsinki.fi>
2012-03-18 17:31:27 +0200

    add .py

---
+++ layer-average.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from gimpfu import *
+
+def layer_average(img, drw):
+  n = len(img.layers)
+  for layer in img.layers:
+    layer.opacity = 1.0/n * 100
+    n = n-1
+  img.flatten()
+
+register(
+        "python_fu_layer_average",
+        "Blend the layers together to create an average image",
+        "Blend the layers together to create an average image",
+        "Oona Raisanen",
+        "Oona Raisanen",
+        "2012",
+        "<Image>/Image/Average Layers",
+        "RGB*, GRAY*",
+        [],
+        [],
+        layer_average)
+
+main()
+
